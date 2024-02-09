8a1690abaa310755ecbc0150ad5d54efe3ee5737
deuro-ki <hero14yki1986@gmail.com>
2024-02-10 02:20:53 +0900

    Goal.vueの追加

--- laravelapp/resources/js/app.js
+++ laravelapp/resources/js/app.js
@@ -5,7 +5,7 @@
  */
 
 require('./bootstrap');
-
+import 'font-awesome/css/font-awesome.min.css';
 window.Vue = require('vue');
 
 /**
@@ -20,6 +20,8 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('goals-component', require('./components/Goal.vue').default);
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
