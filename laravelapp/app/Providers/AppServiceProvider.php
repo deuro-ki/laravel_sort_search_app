ac65f97202f02a2c50b7c66cc31b2c7ee36af8cd
deuro-ki <hero14yki1986@gmail.com>
2024-02-09 18:14:45 +0900

    ログインレイアウトの変更

--- laravelapp/app/Providers/AppServiceProvider.php
+++ laravelapp/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +22,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        // プロダクション環境の場合にのみHTTPSを強制する
+        if (config('app.env') === 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
